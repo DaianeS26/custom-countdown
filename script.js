7f6e5da4a2538abbe80bfb39b49543bf8e909810
Daianes26 <daianeasilva26@gmail.com>
2021-02-03 14:04:58 -0500

    Add comment

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Variables
+
 const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
